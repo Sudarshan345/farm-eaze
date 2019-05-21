e49f83b056760ff5b355bf9f7944c0110b290322
lekhyar <49112395+lekhyar@users.noreply.github.com>
2019-05-21 07:03:42 +0530

    Add files via upload

--- Farm_Eaze/src/com/farmeaze/javafiles/DBOperations.java
+++ Farm_Eaze/src/com/farmeaze/javafiles/DBOperations.java
@@ -1,4 +1,4 @@
-package com.farmeaze.javafiles;
+package Com.UserDetails;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,9 +10,11 @@ import java.util.Map;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
+
+
 public class DBOperations {
 	public static void adddata(String user_name, String mobile_num, String village, String pincode) {
-		Connection conn = DBconnection.getConnection();
+		Connection conn = DBConnection.getDBConnection();
 		Statement stmt;
 
 		try {
@@ -27,7 +29,7 @@ public class DBOperations {
 
 	public static void provideradddata(String provider_name, String mobile_no, String product, String manufacturer_year,
 			String Tractor, String Pincode, String village, String mandal, String district, String state) {
-		Connection conn = DBconnection.getConnection();
+		Connection conn = DBConnection.getDBConnection();
 		Statement stmt;
 
 		try {
@@ -42,159 +44,113 @@ public class DBOperations {
 
 	}
 
-	public static ArrayList<Integer> equip_ids_retrive(int id) {
-		Connection conn = DBconnection.getConnection();
-		ArrayList<Integer> eqplist = new ArrayList<>();
-		try {
-			Statement stmt = conn.createStatement();
-			String query = "select equipment_id from farmeaze.equipment_table where (provider_id=" + id + ")";
-			ResultSet rs = stmt.executeQuery(query);
-			while (rs.next()) {
-				Integer i = rs.getInt(1);
-				eqplist.add(i);
-			}
-			conn.close();
+	
+	 public static List<ProviderBean> providerdetails(String NameofEquipment) {
+		 Statement stmt = null;
+	  ResultSet rs;
+	  Connection conn = DBConnection.getDBConnection();
+	  List<ProviderBean> lis = new ArrayList<ProviderBean>();
+	  try { 
+		  stmt = conn.createStatement();
+			String query = "SELECT provider_name,mobile_no,product,manufacturer_year,Tractor,Pincode,village,mandal,district,state FROM farmeaze.provider_details p INNER join farmeaze.equipment_details e on p.provider_id = e.provider_id where e.NameofEquipment ='"+ NameofEquipment+"'";
+			rs = stmt.executeQuery(query);
+	  while(rs.next()) {
+		  ProviderBean pb = new ProviderBean();
 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		  pb.setprovider_name(rs.getString(1));
+		  pb.setmobile_no(rs.getString(2));
+		pb.setproduct(rs.getString(3));
+		pb.setmanufacturer_year(rs.getString(4));
+		pb.setTractor(rs.getString(5));
+		pb.setPincode(rs.getString(6));
+		pb.setvillage(rs.getString(7));
+	
+		pb.setmandal(rs.getString(8));
+		pb.setdistrict(rs.getString(9));
+		pb.setstate(rs.getString(10));
+		  lis.add(pb);
+	  }
+	  } catch (Exception e) { 
+		  
+	  }
+	return lis;
+	  }
+	 public static List<ProviderBean> retriveprovider() {
+		 Statement stmt = null;
+	  ResultSet rs;
+	  Connection conn = DBConnection.getDBConnection();
+	  List<ProviderBean> lis = new ArrayList<ProviderBean>();
+	  try { 
+		  stmt = conn.createStatement();
+			String query = "SELECT * FROM farmeaze.provider_details";
+			rs = stmt.executeQuery(query);
+	  while(rs.next()) {
+		  ProviderBean pb = new ProviderBean();
 
-		return eqplist;
-
-	}
-
-	public static void delete_equipments(int p_id, int e_id) {
-		Connection conn = DBconnection.getConnection();
-		try {
-			Statement stmt = conn.createStatement();
-			String query = "delete from farmeaze.equipment_table where (provider_id=" + p_id + " and equipment_id="
-					+ e_id + ")";
-			stmt.executeUpdate(query);
-			conn.close();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	public static List<ProviderBean> providerdetails(String NameofEquipment) {
+		  pb.setprovider_name(rs.getString(2));
+		  pb.setmobile_no(rs.getString(3));
+		pb.setproduct(rs.getString(4));
+		pb.setmanufacturer_year(rs.getString(5));
+		pb.setTractor(rs.getString(6));
+		pb.setPincode(rs.getString(7));
+		pb.setvillage(rs.getString(8));
+	
+		pb.setmandal(rs.getString(9));
+		pb.setdistrict(rs.getString(10));
+	/*	pb.setstate(rs.getString(10));*/
+		  lis.add(pb);
+		  System.out.println(pb);
+	  }
+	  } catch (Exception e) { 
+		  
+	  }
+	return lis;
+	  }
+	public static int getProviderId(String provider_name,String mobile_no){
+		Connection conn = DBConnection.getDBConnection();
 		Statement stmt = null;
 		ResultSet rs;
-		Connection conn = DBconnection.getConnection();
-		List<ProviderBean> lis = new ArrayList<ProviderBean>();
+		int pid=0;
+		
 		try {
-			stmt = conn.createStatement();
-			// SELECT
-			/*
-			 * provider_name, mobile_number,
-			 * 
-			 * equipment_name, equipment_description,
-			 * 
-			 * product, manufacturer_year, tractor_reg_number,
-			 * 
-			 * village, mandal, district, state, pincode FROM farmeaze.providers_table p
-			 * 
-			 * INNER JOIN farmeaze.equipment_table e ON p.provider_id = e.provider_id
-			 * 
-			 * WHERE
-			 * 
-			 * e.equipment_name = 'rotavator';
-			 */
-
-			String query = "SELECT provider_name,mobile_number,product,manufacturer_year,pincode,village,mandal,district,state FROM farmeaze.providers_table p "
-					+ "INNER join farmeaze.equipment_table e on p.provider_id = e.provider_id where e.equipment_name ='"
-					+ NameofEquipment + "'";
+			String query = "SELECT provider_id FROM farmeaze.provider_details where provider_name='"+provider_name+"' and mobile_no='"+mobile_no+"'";
+	stmt=conn.createStatement();
 			rs = stmt.executeQuery(query);
-			while (rs.next()) {
-				System.out.println(".....entered.... into while");
-				ProviderBean pb = new ProviderBean();
-
-				pb.setprovider_name(rs.getString(1));
-				pb.setmobile_no(rs.getString(2));
-				pb.setproduct(rs.getString(3));
-				pb.setmanufacturer_year(rs.getString(4));
-				pb.setPincode(rs.getString(5));
-				pb.setvillage(rs.getString(6));
-
-				pb.setmandal(rs.getString(7));
-				pb.setdistrict(rs.getString(8));
-				pb.setstate(rs.getString(9));
-				lis.add(pb);
-			}
-		} catch (Exception e) {
-
+		
+		while(rs.next()) {
+			
+			
+			 pid =rs.getInt(1);
+			
+			
+			//System.out.println(pid);
 		}
-		return lis;
-
-	}
-
-	public static List<TimeSlots> timeslots(int id) {
-		Statement stmt = null;
-		ResultSet rs;
-		Connection conn = DBconnection.getConnection();
-		List<TimeSlots> lis = new ArrayList<TimeSlots>();
-		try {
-			stmt = conn.createStatement();
-
-			String query = "SELECT farmeaze.slot_table.from_date,farmeaze.slot_table.from_time,farmeaze.slot_table.to_date,farmeaze.slot_table.to_time "
-					+ "FROM farmeaze.slot_table where provider_id=" + id + "";
-			rs = stmt.executeQuery(query);
-			while (rs.next()) {
-				System.out.println(".....entered.... into while");
-				TimeSlots pb = new TimeSlots();
-
-				pb.setfromdate(rs.getString(1));
-				pb.settodate(rs.getString(3));
-				pb.setfromtime(rs.getString(2));
-				pb.settotime(rs.getString(4));
-				lis.add(pb);
-
-			}
-		} catch (Exception e) {
-
-		}
-		return lis;
-
-	}
-
-	public static void timeDB(String fd, String ft, String td, String tt, int id) {
-		Connection conn = DBconnection.getConnection();
-		Statement stmt;
-
-		try {
-			stmt = conn.createStatement();
-			String Query = "INSERT INTO `farmeaze`.`slot_table` (`provider_id`, `from_date`, `from_time`, `to_date`, `to_time`) VALUES ('"
-					+ id + "', '" + fd + "', '" + ft + "', '" + td + "', '" + tt + "');";
-			stmt.executeUpdate(Query);
-			conn.close();
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-
+		return pid;
+		
 	}
-
-	public static ArrayList<String> equip_dispaly() {
-
-		Statement stmt = null;
-		ResultSet rs;
-		Connection conn = DBconnection.getConnection();
-		ArrayList<String> lis = new ArrayList<>();
-		try {
-			stmt = conn.createStatement();
-
-			String query = "SELECT farmeaze.list_of_equipments.eqp_name FROM farmeaze.list_of_equipments;";
+	 public static List<EquipmentBean> retriveequipmentbaseprovider(int provider_id) {
+		 Statement stmt = null;
+	  ResultSet rs;
+	  Connection conn = DBConnection.getDBConnection();
+	  List<EquipmentBean> lis = new ArrayList<EquipmentBean>();
+	  try { 
+		  stmt = conn.createStatement();
+			String query = "SELECT * FROM farmeaze.equipment_details where provider_id='"+provider_id+"'";
 			rs = stmt.executeQuery(query);
-			while (rs.next()) {
-				String e1 = rs.getString(1);
-				lis.add(e1);
-			}
-		} catch (Exception e) {
-
-		}
-		return lis;
-
-	}
+	  while(rs.next()) {
+		
+		EquipmentBean eb= new EquipmentBean();
+		eb.setNameofEquipment(rs.getString(2));
+		eb.setDescription(rs.getString(3));
+		lis.add(eb);
+		System.out.println(eb);
+	  }
+	  } catch (Exception e) { 
+		  
+	  }
+	return lis;
+	  }
 }
---
+++ Farm_Eaze/src/com/farmeaze/javafiles/EquipmentBean.java
@@ -0,0 +1,20 @@
+package Com.UserDetails;
+
+public class EquipmentBean {
+	public String NameofEquipment;
+	public String Description;
+
+public String getUsername() {
+	return NameofEquipment;
+}
+public void setNameofEquipment(String NameofEquipment) {
+	this.NameofEquipment=NameofEquipment;
+}
+public String getNameofEquipment() {
+	return Description;
+}
+public void setDescription(String Description) {
+	this.Description =Description;
+}
+
+}
